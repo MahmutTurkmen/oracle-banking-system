1d8cfda585612d7a5cb42ac561641d4d86848e2e
MahmutTurkmen <150715109+MahmutTurkmen@users.noreply.github.com>
2026-03-03 18:54:06 +0300

    Add transactions table with constraints and trigger
    Created the transactions table with necessary constraints and triggers.

--- schema/03_transactions.sql
+++ schema/03_transactions.sql
@@ -1,1 +1,25 @@
+-- transactions(işlemler) tablosunu oluşturdum.
+create table transactions (
+transactions_id number primary key,
+from_account_id number,
+to_account_id number,
+amount_number(15, 2) check (amount > 0),
+transaction_type varchar2(20)
+check (transaction_type in ('TRANSFER', 'DEPOSIT', 'WITHDRAW')),
 
+transaction_date date default sysdate, status varchar2(20)
+check (status in ('SUCCESS', 'FAILED')),
+
+constraint fk_from_account foreign key (from_account_id)
+references accounts(account_id),
+
+constraint fk_to_account foreign key(to_account_id)
+references accounts(account_id));
+
+-- sequence oluşturdum.
+create sequence transactions_seq start with 1 increment by 1;
+
+-- trigger oluşturdum. 
+create or replace trigger transactions_trigger before insert on transactions
+for each row begin :NEW.transaction_id := transactions_seq.NEXTVAL;
+end; /
